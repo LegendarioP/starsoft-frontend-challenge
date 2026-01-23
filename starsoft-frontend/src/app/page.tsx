017c139da0f12ec58706f5cc9c107c5871da8564
Pablo Luan <pablo-544@hotmail.com>
2026-01-22 21:34:45 -0300

    feat: add Footer component for copyright notice

--- starsoft-frontend/src/app/page.tsx
+++ starsoft-frontend/src/app/page.tsx
@@ -1,9 +1,16 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-background text-white">
+    <div className="flex min-h-screen flex-col bg-background text-white">
       <Header />
+
+      <main className="flex flex-1 flex-row flex-wrap gap-6.25 px-[8.53125rem] py-47.25">
+
+
+      </main>
+      <Footer />
 
     </div>
   );
---
+++ starsoft-frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full text-sm leading-3.5 py-6.25 flex justify-center text-white/45">
+      STARSOFT © TODOS OS DIREITOS RESERVADOS
+    </footer>
+  )
+}
