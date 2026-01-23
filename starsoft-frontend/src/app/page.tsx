f6d50604341bd8bc2d35bd18b3981480fa5146f7
Pablo Luan <pablo-544@hotmail.com>
2026-01-22 21:56:33 -0300

    feat: add ProductCard component with image and description

--- starsoft-frontend/src/app/page.tsx
+++ starsoft-frontend/src/app/page.tsx
@@ -1,13 +1,16 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import ProductCard from "../components/ProductCard";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background text-white">
       <Header />
 
-      <main className="flex flex-1 flex-row flex-wrap gap-6.25 px-[8.53125rem] py-47.25">
-
+      <main className="flex flex-1 flex-row flex-wrap justify-center gap-6.25 px-[8.53125rem] py-47.25">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <ProductCard key={index} />
+        ))}
 
       </main>
       <Footer />
