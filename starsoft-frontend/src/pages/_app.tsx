227f601d13eb744a86d1b44aced7676f04f7abe6
Pablo Luan <pablo-544@hotmail.com>
2026-01-27 18:08:40 -0300

    feat: add next-sitemap configuration and update package dependencies for sitemap generation

--- starsoft-frontend/src/pages/_app.tsx
+++ starsoft-frontend/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { persistor, store } from "@/store";
 import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import Footer from "../components/Footer";
@@ -18,20 +19,27 @@ const poppins = Poppins({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={poppins.className}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <QueryClientProvider client={queryClient}>
-            <StyledComponentsRegistry>
-              <PageContainer>
-                <Header />
-                <Component {...pageProps} />
-                <Footer />
-              </PageContainer>
-            </StyledComponentsRegistry>
-          </QueryClientProvider>
-        </PersistGate>
-      </Provider>
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#232323" />
+      </Head>
+      <div className={poppins.className}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <QueryClientProvider client={queryClient}>
+              <StyledComponentsRegistry>
+                <PageContainer>
+                  <Header />
+                  <Component {...pageProps} />
+                  <Footer />
+                </PageContainer>
+              </StyledComponentsRegistry>
+            </QueryClientProvider>
+          </PersistGate>
+        </Provider>
+      </div>
+    </>
   );
 }
