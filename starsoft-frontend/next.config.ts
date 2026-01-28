edeef6324cfb70a3ff0d3ad2cf94db3eba376955
Pablo Luan <pablo-544@hotmail.com>
2026-01-27 21:48:13 -0300

    feat: add image remote patterns to next.config for external image support

--- starsoft-frontend/next.config.ts
+++ starsoft-frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [new URL('https://softstar.s3.amazonaws.com/items/**')],
+  },
 };
 
 export default nextConfig;
